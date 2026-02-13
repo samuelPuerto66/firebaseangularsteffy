b2c538ef7e2bf8d33e36dfe5b7ca6fce2afd859e
=Juan <=juanstebanriveros@gmail.com>
2026-02-13 14:28:51 -0500

    Initial commit: Chat Angular con Firebase y Google Auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AuthGuard } from '@angular/fire/auth-guard';
+import { authGuard } from './guard/auth-guard';
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/auth',
+        pathMatch: 'full'
+    },{
+        path:'auth',
+        loadComponent:() => import ('./components/auth/auth').then(m => m.Auth),
+        title: "Inicio de Sesion"
+    },{
+        path:'chat',
+         loadComponent:() => import ('./components/chat/chat').then(m => m.Chat),
+         title: "Chat con tu agente IA",
+         canActivate: [authGuard]
+    },{
+        path:"**",
+        redirectTo:"/auth"
+    }
+];
