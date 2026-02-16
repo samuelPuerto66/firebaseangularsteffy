f0e0a69f98bb7389d69f4eeb367fbe493de047e2
Usuario <you@example.com>
2026-02-16 17:00:11 -0500

    Guardar cambios y archivos nuevos

--- src/app/components/auth/auth.ts
+++ src/app/components/auth/auth.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-auth',
@@ -8,6 +11,57 @@ import { Component } from '@angular/core';
 })
 export class Auth {
 MensajeError="."
-Autenticando=true
-IniciarSesionConGoogle(){}
+Autenticando=false
+
+//inject de dependencias
+
+private authservice = inject(AuthService)
+private router = inject(Router)
+
+
+//funcion que revise la autenticacion = asincrona
+
+async IniciarSesionConGoogle (): Promise <void>{
+  this.Autenticando = true
+  this.MensajeError = ""
+
+   try {
+    //falta implementar el servicio
+
+
+
+
+
+
+    //vamos a simular un usuario ya creado
+    let usuarios=null
+    usuarios = await new Promise ((resolve) =>{
+      setTimeout(()=>resolve({nombre:'usuario de prueba'}),1000)
+  })
+  if (usuarios){
+    await this.router.navigate(['/chat'])
+  } else{
+    this.MensajeError = " Hubo un error al Autenticar "
+    console.error(" Error al autentivar con try ")
+  }
+  } catch (error: any){
+    
+    //validacion de algunos posibles errores
+    if(error.code === "auth/popup-closed-by-user"){
+      console.error('Error: Cerraste la ventana emergente')
+    } else if (error.code =="auth/popup-blocked"){
+      console.error('El navegador bloqueo la ventana emergente')
+    }else if (error.code =='auth/network-request-failed'){
+      console.error('Problemas con la conexion a internet')
+    }
+
+   }finally{
+    this.Autenticando = false
+   
+
+
+   }
+   
+  }
+  
 }
